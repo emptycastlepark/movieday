0e4d77216d8b26e367133f9e4c9ca0ac5ffcaa07
Donghyeok-Lee <lee33843@gmail.com>
2020-06-16 09:18:47 +0900

    user detail update

--- movieday/movieday/settings.py
+++ movieday/movieday/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import json
+
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,10 +153,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-import json
-
-from django.core.exceptions import ImproperlyConfigured
 
 with open('movieday/secret.json') as f:
     secret = json.loads(f.read())
